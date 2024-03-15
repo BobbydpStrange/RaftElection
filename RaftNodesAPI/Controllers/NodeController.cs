8ee10e3428f41ce1d1f8f03211c69f227b0bc1db
bobby <bobby@DESKTOP-6V2GR6M>
2024-03-15 00:55:37 -0600

    added more methods to the nodecontroller

--- RaftNodesAPI/Controllers/NodeController.cs
+++ RaftNodesAPI/Controllers/NodeController.cs
@@ -7,27 +7,56 @@ namespace RaftNodesAPI.Controllers;
 public class NodeController : Controller
 {
     private readonly Node _node;
+    private List<string> _nodeLocations;
     public NodeController(Node node)
     {
         _node = node;
+        _nodeLocations = Environment.GetEnvironmentVariable("NODES_LOCATION")?.Split(", ")?.ToList();
     }
 
-    /* Get state
-     * post vote
-     * get leader
-     * get log?
-     */
-    [HttpGet("GetResponse")]
-    public async Task<bool> GetResponse()
+    [HttpGet("GetLatestLog")]
+    public async Task<Tuple<string, int>> GetLatestLog()
+    { 
+        _node.history.TryGetValue("LogVersion", out Tuple<string, int> result);
+        return result;
+    }
+    [HttpGet("GetNode")]
+    public async Task<Node> GetNodeInfo()
     {
-
-        return true;
+        return _node;
+    }
+    [HttpGet("GetVote")]
+    public async Task<bool> GetVote(int term, Guid Candidateid)
+    {
+        var response = _node.Vote(term, Candidateid);
+        return response;
     }
 
-    [HttpPost("PostResponse")]
-    public async Task <bool> PostResponse()
+    [HttpPost("PostElection")]
+    public async Task <bool> PostElection()
     {
-        return true;
+        try
+        {
+            if (_node.state =="candidate")
+            {
+                var httpClient = new HttpClient();
+                var voteCount = 0;
+                var majority = ((_nodeLocations.Count())/2)+1 ;
+                for(var i = 0; i < _nodeLocations.Count(); i++)
+                {
+                    var response = await httpClient.GetAsync($"{_nodeLocations[i]}/GetVote?term={_node.currentTerm}&CandidateId={_node.nodeid}");
+                    if (response.Content.ReadAsStringAsync().Result == "true")
+                    {
+                        voteCount++;
+                    }
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }catch (Exception ex) { Console.WriteLine(ex.Message);  return false; }
     }
 
 }
